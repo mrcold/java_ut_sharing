d01abac1fcb3867c6d41e970441fdfacfd135ef9
Leo Han <lhan@freewheel.tv>
2016-12-27 10:43:19 +0800

    add expect exeption sample

--- junit4-samples/src/test/java/rule/TemporaryFolderRuleTest.java
+++ junit4-samples/src/test/java/rule/TemporaryFolderRuleTest.java
@@ -3,15 +3,22 @@ package rule;
 import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
 
+import static org.hamcrest.core.Is.is;
+
 public class TemporaryFolderRuleTest
 {
     @Rule
     public TemporaryFolder tempFolder = new TemporaryFolder();
+
+    @Rule
+    public ExpectedException exception = ExpectedException.none();
 
     @Test
     public void countsAssets() throws IOException
@@ -30,5 +37,12 @@ public class TemporaryFolderRuleTest
             File asset = new File(assets, String.format("asset-%d.mpg", index));
             Assert.assertTrue("Asset couldn't be created.", asset.createNewFile());
         }
+    }
+
+    @Test
+    public void testError()
+    {
+        exception.expect(ArrayIndexOutOfBoundsException.class);
+        String s = new String[]{}[1];
     }
 }
